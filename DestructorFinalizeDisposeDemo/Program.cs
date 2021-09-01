e63755a9f4102f7c2ca97d1d7657e229add04901
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:52:14 +0530

    Add project files.

---
+++ DestructorFinalizeDisposeDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DestructorFinalizeDisposeDemo
+{
+    class Complex
+    {
+          int real, img;
+        public Complex()
+        {
+            real = 0;
+            img = 0;
+        }
+        public void SetValue(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+         public void DisplayValue()
+        {
+            Console.WriteLine("Real = " + real);
+            Console.WriteLine("Imaginary = " + img);
+        }
+         ~Complex()
+        {
+            Console.WriteLine("Destructor was called");
+        }
+
+    } 
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Complex C = new Complex();
+             C.SetValue(2, 3);
+             C.DisplayValue();
+        } 
+
+    } 
+
+}
---
+++ Disposedemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Disposedemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class ResourceManagement : IDisposable
+
+        {
+
+            public ResourceManagement()
+
+            { }
+
+            private bool IsDisposed = false;
+
+            public void Free()
+
+            {
+
+                if (IsDisposed)
+
+                    throw new System.ObjectDisposedException("Object Name");
+
+            }
+
+          
+
+            public void Dispose()
+
+            {                Dispose(true);
+
+               
+
+                GC.SuppressFinalize(this);
+
+            }
+
+            ~ResourceManagement()
+
+            {
+
+ 
+                Dispose(false);
+
+            }
+
+            protected virtual void Dispose(bool disposedStatus)
+
+            {
+
+                if (!IsDisposed)
+
+                {
+
+                    IsDisposed = true;
+
+                
+
+                    if (disposedStatus)
+
+                    {
+
+                 
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
